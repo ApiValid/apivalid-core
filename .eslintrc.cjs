f3516f09480fd119804b5644a179edfa10470ec2
ducng99 <tom@ducng.dev>
2023-03-04 23:33:43 +1300

    chore: Added dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard-with-typescript',
+    ],
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json'
+    },
+    plugins: [
+        'react'
+    ],
+    rules: {
+        indent: ['error', 4],
+        '@typescript-eslint/indent': ['error', 4],
+        semi: ['error', 'always'],
+        '@typescript-eslint/semi': ['error', 'always'],
+        'comma-dangle': 0,
+        'space-before-function-paren': ['error', 'never'],
+        '@typescript-eslint/space-before-function-paren': ['error', 'never'],
+        '@typescript-eslint/parameter-properties': 'error',
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        '@typescript-eslint/member-delimiter-style': 'off',
+    }
+};
