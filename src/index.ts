d3b75b61089c40300daacf806d975890c51e7b08
ducng99 <tom@ducng.dev>
2023-03-05 18:50:42 +1300

    Added index file for publishing

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export { default as TestSuite } from './models/testing/TestSuite';
+export { default as TestSuitesController } from './controllers/testing/TestSuitesController';
+export { default as TestCase } from './models/testing/TestCase';
+export { default as TestCasesController } from './controllers/testing/TestCasesController';
+export { default as TestStep } from './models/testing/TestStep';
+export { default as TestStepsController } from './controllers/testing/TestStepsController';
+export { default as ApiRequestTestStep } from './models/testing/predefined-steps/ApiRequest/ApiRequestTestStep';
+export { default as EHttpMethods } from './models/common/http/EHttpMethods';
+export { default as ERequestContentTypes } from './models/common/http/ERequestContentTypes';
+export { default as RequestBody } from './models/common/http/RequestBody';
